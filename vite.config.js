6d7217d574a120d4a0b4d35bfd3605c2688eabf8
Samyabrata roy <samyaroy00@gmail.com>
2025-05-20 04:09:08 +0530

    Test Homepage

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
-  base: '/chi-square/',  // ← replace with actual repo name
+  plugins: [react()],
+  base: '/chi-square/',
 });
